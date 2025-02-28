3d122da09683a0b248215263c920f2c1a40b9ba9
Shantanu-Muluk <shantanumuluk9878@gmail.com>
2025-02-28 13:43:54 +0530

    Reading and writing from file

--- readWriteFile.js
+++ readWriteFile.js
@@ -1,28 +1,55 @@
-let http = require("http");
+const http = require("http");
+const fs = require("fs");
 
-let server = http.createServer((req,res)=>{
-
+const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
 
-    if(url === "/"){
-
+    if (url === "/") {
         res.setHeader("Content-Type", "text/html");
-
-        res.end(
-            `<form action="/message" method = "POST">
-                <lable>Name: </lable>
-                <input type="text", name="username"></input>
+        res.end(`
+            <form action="/message" method="POST">
+                <label>Name: </label>
+                <input type="text" name="username" />
                 <button type="submit">Add</button>
             </form>
-            `
-        )
-    }else if(url === "/message"){
-        res.setHeader("Content-Type", "text/html");
-        res.end("<h1>Thank you! Your form has been submitted.</h1>");
-    }
-})
+        `);
+    } else if (url === "/message" && method === "POST") {
+        let dataChunks = [];
 
-server.listen(3000, ()=>{
-    console.log("server is running")
-})
+        req.on("data", (chunk) => {
+            dataChunks.push(chunk);
+        });
+
+        req.on("end", () => {
+            const combinedBuffer = Buffer.concat(dataChunks);
+            const parsedData = combinedBuffer.toString();
+            const username = parsedData.split("=")[1]; // Extracts the value after "username="
+
+            // Writing to file
+            fs.writeFile("formVal.txt", username, (err) => {
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.end("Error saving data");
+                    return;
+                }
+                // Redirect after writing to file
+                res.statusCode = 302;
+                res.setHeader("Location", "/");
+                res.end();
+            });
+        });
+    } else {
+       if(url === "/read"){
+        fs.readFile("formVal.txt",(err,data)=>{
+            console.log(data.toString())
+            res.end(`<h1>${data.toString()}</h1>`)
+        })
+       }
+    }
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
